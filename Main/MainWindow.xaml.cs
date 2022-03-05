c7142f9bb1cc84d8bd23542a19efc875f8df4e07
milán bajár <bajarmilan20010205@outlook.hu>
2022-03-05 01:25:21 +0100

    Login View elkészítése

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -1,16 +1,54 @@
 ﻿using System.Windows;
-
+using SD_SOFT_DAL;
+using MaterialDesignThemes.Wpf;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Windows.Application;
+using System.Windows.Input;
 
 namespace Main
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
+
+
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        //Fekete téma beállítása
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if(IsDarkTheme=theme.GetBaseTheme()==BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+                
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+        #region alkalmazás bezárása
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
         }
     }
 }
